e16b53c82aa3dec3e4a19be9fc30131a12f4bc47
arunimams7559 <arunimams152003@gmail.com>
2025-05-16 16:53:20 +0530

    second commit

--- routes/chatroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChatMessages, sendMessage } = require('../controllers/chat');
-
-router.get('/messages', getChatMessages);
-router.post('/send', sendMessage);
-
-module.exports = router;
--- routes/userroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, getUserProfile } = require('../controllers/users');
-
-router.get('/', getUsers);
-router.get('/profile', getUserProfile);
-
-module.exports = router;
